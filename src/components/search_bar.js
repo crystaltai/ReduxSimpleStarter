e48092814fa8c8362da65b5b0963fb4d0db85c04
Crystal Tai <crystaltai315@gmail.com>
2018-07-30 20:56:59 -0700

    initial commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'; // this pulls the Component property and saves it to a variable name Component
+
+// first declare a new class with the name SearchBar
+// then give it access to all of the functionality that React.Component has
+class SearchBar extends Component {
+  // the constructor method is how we initialize state
+  constructor(props) {
+    super(props);
+    this.state = { term: '' };
+  }
+  render() {
+    // we have an 'input' element that we added an event handler to
+    // the event hander triggers whenever the event occurs
+    // onChange is a React property that we pass the event handler to
+    // this.setState will update 'this.state'
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={ event => this.setState({ term: event.target.value }) } />
+      </div>
+    );
+  }
+}
+
+// now any file that imports search_bar.js, it will get the SearchBar component
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-import App from './components/app';
-import reducers from './reducers';
+// Youtube API key - will allow us to make requests to Youtube
+const API_KEY = 'AIzaSyBTb5sKQKEqkiJN3pftvlxyWVMBO7pPgpY';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+// Create a new component. This component should produce some HTML
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // this.setState({ videos: videos });
+    });
+  }
+  ac
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
